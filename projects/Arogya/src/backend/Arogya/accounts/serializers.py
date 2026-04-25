5883f701f2259fb1e09d1b3db95661dbdf653370
Aashish012-rgb <182605557+Aashish012-rgb@users.noreply.github.com>
2026-04-25 23:27:53 +0545

    fixed the errors in the account

--- projects/Arogya/src/backend/Arogya/accounts/serializers.py
+++ projects/Arogya/src/backend/Arogya/accounts/serializers.py
@@ -1,7 +1,24 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'email']
