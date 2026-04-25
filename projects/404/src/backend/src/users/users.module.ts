4357581d9cdc1e1ca9afe4cec49dbc0efbae3730
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-04-25 23:28:11 +0545

    done the complete crud for the specializations and used specializations in users also

--- projects/404/src/backend/src/users/users.module.ts
+++ projects/404/src/backend/src/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma.service';
+import { SpecializationsModule } from '../specializations/specializations.module';
 
 @Module({
+  imports: [SpecializationsModule],
   controllers: [UsersController],
   providers: [UsersService, PrismaService],
   exports: [UsersService],
