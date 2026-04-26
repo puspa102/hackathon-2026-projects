105aa65eb5a849e79a12fce43420dcbae530c613
sandeshgyw <sandeshgyanwali@gmail.com>
2026-04-25 20:13:48 -0500

    initialized notification service on main

--- projects/MediMeal/src/medimeal/lib/main.dart
+++ projects/MediMeal/src/medimeal/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:medimeal/services/notification_service.dart';
 import 'screens/main_navigation_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  await NotificationService.init();
 
   runApp(const MediMealApp());
 }
